e0aa1d0697ba602435a9cc1624020d2cff17db16
RoyEden <roy.ede@gmail.com>
2022-12-05 13:28:03 -0300

    Force seek playhead to handle ended events

--- dist/plyr.js
+++ dist/plyr.js
@@ -8105,7 +8105,7 @@ typeof navigator === "object" && (function (global, factory) {
       const inputIsValid = is.number(input) && input > 0;
 
       // Set
-      this.media.currentTime = inputIsValid ? Math.min(Math.floor(input), Math.floor(this.duration)) : 0;
+      this.media.currentTime = inputIsValid ? Math.min(Math.floor(input), Math.floor(this.duration) - 1) : 0;
 
       // Logging
       this.debug.log(`Seeking to ${this.currentTime} seconds`);
